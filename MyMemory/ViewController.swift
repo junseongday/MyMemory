ce6a31bb3dd9752d97b62deee1d0d1441573a297
junseongday <junseongday@gmail.com>
2019-12-27 00:14:35 +0900

    Initial Commit

---
+++ MyMemory/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MyMemory
+//
+//  Created by JSMAC on 2019/12/26.
+//  Copyright © 2019 JSPRO. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
