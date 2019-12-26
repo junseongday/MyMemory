1f7bb1b480a83453b1885594c8860b280d75f2a7
junseongday <junseongday@gmail.com>
2019-12-27 07:10:16 +0900

    화면구성

---
+++ MyMemory/MemoCell.swift
@@ -0,0 +1,24 @@
+//
+//  MemoCell.swift
+//  MyMemory
+//
+//  Created by JSMAC on 2019/12/27.
+//  Copyright © 2019 JSPRO. All rights reserved.
+//
+
+import UIKit
+
+class MemoCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ MyMemory/MemoFormVC.swift
@@ -0,0 +1,30 @@
+//
+//  MemoFormVC.swift
+//  MyMemory
+//
+//  Created by JSMAC on 2019/12/27.
+//  Copyright © 2019 JSPRO. All rights reserved.
+//
+
+import UIKit
+
+class MemoFormVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
