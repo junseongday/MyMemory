244d63c509a54f95b0f5bf8e4bbf1e99317487f4
junseongday <junseongday@gmail.com>
2020-02-18 23:58:52 +0900

    4장종료

---
+++ MyMemory/ProfileVC.swift
@@ -0,0 +1,73 @@
+//
+//  ProfileVC.swift
+//  MyMemory
+//
+//  Created by JSMAC on 2020/02/18.
+//  Copyright © 2020 JSPRO. All rights reserved.
+//
+
+import UIKit
+class ProfileVC: UIViewController, UITableViewDelegate, UITableViewDataSource{
+    let profileImage = UIImageView()
+    let tv = UITableView()
+    
+    override func viewDidLoad() {
+        let backBtn = UIBarButtonItem(title: "닫기", style: .plain, target: self, action: #selector(close(_:)))
+        self.navigationItem.leftBarButtonItem = backBtn
+        
+        let bg = UIImage(named: "profile-bg")
+        let bgImg = UIImageView(image: bg)
+        bgImg.frame.size = CGSize(width: bgImg.frame.width, height: bgImg.frame.height)
+        bgImg.center = CGPoint(x: self.view.frame.width / 2, y: 40)
+        
+        bgImg.layer.cornerRadius = bgImg.frame.width / 2
+        bgImg.layer.borderWidth = 0
+        bgImg.layer.masksToBounds = true
+        self.view.addSubview(bgImg)
+        
+        
+        let image = UIImage(named: "account.jpg")
+        
+        self.profileImage.image = image
+        self.profileImage.frame.size = CGSize(width: 100, height: 100)
+        self.profileImage.center = CGPoint(x: self.view.frame.width/2, y: 270)
+        
+        self.profileImage.layer.cornerRadius = self.profileImage.frame.width / 2
+        self.profileImage.layer.borderWidth = 0
+        self.profileImage.layer.masksToBounds = true
+        self.view.addSubview(self.profileImage)
+        
+        //tableview
+        self.tv.frame = CGRect(x: 0, y: self.profileImage.frame.origin.y + self.profileImage.frame.height + 20, width: self.view.frame.width, height: 100)
+        self.tv.dataSource = self
+        self.tv.delegate = self
+        self.view.addSubview(self.tv)
+        self.navigationController?.navigationBar.isHidden = true
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 2
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell(style: .value1, reuseIdentifier: "cell")
+        cell.textLabel?.font = UIFont.systemFont(ofSize: 14)
+        cell.detailTextLabel?.font = UIFont.systemFont(ofSize: 13)
+        cell.accessoryType = .disclosureIndicator
+        switch indexPath.row {
+        case 0:
+            cell.textLabel?.text = "이름"
+            cell.detailTextLabel?.text = "꼼꼼한 준성 씨"
+        case 1:
+            cell.textLabel?.text = "계정"
+            cell.detailTextLabel?.text = "junseongday@gmail.com"
+        default:
+            ()
+        }
+        return cell
+    }
+    
+    @objc func close(_ sender: Any) {
+        self.presentingViewController?.dismiss(animated: true, completion: nil)
+    }
+}
--- MyMemory/SideBarVC.swift
+++ MyMemory/SideBarVC.swift
@@ -72,6 +72,12 @@ class SideBarVC: UITableViewController {
             let target  = self.revealViewController().frontViewController as! UINavigationController
             target.pushViewController(uv!, animated: true)
             self.revealViewController().revealToggle(self)
+        } else if indexPath.row == 5 {
+            let uv = self.storyboard?.instantiateViewController(withIdentifier: "_Profile")
+            let target = self.revealViewController()?.frontViewController as! UINavigationController
+            target.present(uv!, animated: true) {
+                self.revealViewController()?.revealToggle(self)
+            }
         }
     }
 }
